f965128d29ba5aa0e06e087bad399e439953eb03
谢雷君 <xieleijun@xieleijundeMacBook-Pro-6.local>
2022-12-19 07:56:43 +0800

    first commit

---
+++ src/components/Sheet.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(() => {
+  new window.XSheet("#demo", {
+    workConfig: {
+      body: {
+        sheets: [window["forecast"]],
+      },
+    },
+  });
+});
+</script>
+
+<template>
+  <div id="demo"></div>
+</template>
+
+<style scoped>
+#demo {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+export {};
+declare global {
+  interface Window {
+    XSheet: any;
+    forecast: any;
+  }
+}
