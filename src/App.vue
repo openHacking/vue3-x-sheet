90ecf51da306c71bbf67e481261c7d0e7d43e5e3
openHacking <openHacking@126.com>
2022-12-19 22:14:50 +0800

    sheet.vue and forecast.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Sheet from "./components/Sheet.vue";
+import Forecast from "./components/Forecast.vue";
 </script>
 
 <template>
   <Sheet />
+  <!-- <Forecast /> -->
 </template>
 
 <style scoped>
--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -2,13 +2,41 @@
 import { onMounted } from "vue";
 
 onMounted(() => {
-  new window.XSheet("#demo", {
+  const settings = {
+    // 工作簿配置
     workConfig: {
+      // 主体配置
       body: {
-        sheets: [window["forecast"]],
+        // 工作表主体配置
+        sheets: [
+          {
+            // 工作表名称
+            name: "sheet1",
+            // 表格配置
+            tableConfig: {
+              // 表格数据，二维数组
+              data: [
+                [
+                  {
+                    // 文本内容
+                    text: "Text1",
+                  },
+                  {
+                    // 文本内容
+                    text: "Text2",
+                  },
+                ],
+              ],
+              // 合并单元格
+              merge: { merges: ["A2:B3"] },
+            },
+          },
+        ],
       },
     },
-  });
+  };
+
+  new window.XSheet("#demo", settings);
 });
 </script>
 
